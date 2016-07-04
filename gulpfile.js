30f9d9d17e317e6deb92068d8171bb1aa8cd3b44
Misha Pesterev <mpesterev@radaris.com>
2016-07-04 16:22:04 +0300

    social buttons

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,48 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	  sass = require('gulp-sass'),
+	  cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin');
 
-gulp.task('sass', function() {
-  return gulp.src('../../rd/rd/www/css/sass/main.scss')
+/*Radaris needed*/
+gulp.task('radaris', function() {
+  return gulp.src('/rd/rd/www/css/sass/main.scss')
   .pipe(sass())
-  .pipe(gulp.dest('../../rd/rd/www/css'))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('/rd/rd/www/css'))
+});
+
+gulp.task('themeRadaris', function() {
+  return gulp.src('/rd/rd/www/css/sass/bootstrap-theme.scss')
+  .pipe(sass())
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('/rd/rd/www/css'))
+});
+
+gulp.task('bamCss', function() {
+  return gulp.src('/rd/rd/www/css/sass/bam_insider.scss')
+  .pipe(sass())
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('/rd/rd/www/css'))
+});
+
+/*Radaris needed END*/
+
+gulp.task('compressImages', function() {
+  gulp.src('/rd/rd/www/img/*.png')
+    .pipe(imagemin())
+    .pipe(gulp.dest('/rd/rd/www/images/'))
+});
+
+gulp.task('forum-rehold', function() {
+  return gulp.src('/rd/rd/www/css/forum/sass/forum.scss')
+  .pipe(sass())
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('/rd/rd/www/css'))
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('/rd/rd/www/css/sass/*.scss', ['radaris', 'bamCss', 'themeRadaris']);
 });
