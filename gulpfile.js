371ed58a1daecc10f25d493133ede3d003a222fd
Michael Pesterev <mpesterev@di5-mpesterev.r-k.radaris.com>
2016-10-04 10:41:41 -0400

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//var concat = require('gulp-concat');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
 var sassGlob = require('gulp-sass-glob');
 var cleanCSS = require('gulp-clean-css');
 var imagemin = require('gulp-imagemin');
@@ -38,6 +39,20 @@ gulp.task('sass', function () {
     .pipe(sass({outputStyle: 'compressed', includePaths: [sassPath], errLogToConsole: true}).on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest(cssPath));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        jsPath + 'raw/foundation/foundation.core.js',
+        jsPath + 'raw/foundation/foundation.util.*.js',
+        jsPath + 'raw/foundation/foundation.tooltip.js'
+    ])
+    //.pipe(uglify('app.min.js', {
+    //  outSourceMap: true
+    //}))
+    //.pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(jsPath));
 });
 
 gulp.task('watch', function(event) {
