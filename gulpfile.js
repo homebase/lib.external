8194b998922e6376e945b83f916bed94b2d70c0e
Oleg Lipovoi <o.lipovoi@radaris.com>
2018-03-06 18:40:30 +0200

    Updated bootstrap4 to stable version.

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,10 @@ if (process.argv[3] == '--c1') {
     basePath = '/rd/vhosts/wellnut/www/';
 } if (process.argv[3] == '--arrestfacts') {
     basePath = '/rd/vhosts/arrestfacts/www/';
+} if (process.argv[3] == '--licenseswhois') {
+    basePath = '/rd/vhosts/license/licensewhois.com/www/';
+} if (process.argv[3] == '--wp') {
+    basePath = '/rd/vhosts/rdc/wp.com/www/';
 }
 
 // Paths
