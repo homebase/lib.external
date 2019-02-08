d186274c145cae18af2831097f6d5e4d4da69529
Michael Glyebov <mglyebov@radaris.com>
2019-02-08 12:56:49 +0200

    add trustoria rule

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,8 @@ if (process.argv[3] == '--c1') {
     basePath = '/rd/vhosts/address/homemetry.com/www/';
 } if (Object.values(process.argv).indexOf('--rehold') > -1) {
     basePath = '/rd/vhosts/address/rehold.com/www/';
+} if (Object.values(process.argv).indexOf('--trustoria') > -1) {
+    basePath = '/rd/vhosts/trustoria.com/www/';
 }
 
 // Paths
