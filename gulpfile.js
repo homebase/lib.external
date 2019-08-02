0af105e2ed8bce01e525e7cc9257193faae2c56f
Oleg Lipovoi <o.lipovoi@radaris.com>
2019-08-02 14:47:06 +0300

    Fixed trustoris.com: RPG-1642

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ if (process.argv[3] == '--c1') {
 } if (process.argv[3] == '--cityzor') {
     basePath = '/rd/vhosts/cityzor/www/';
 } if (Object.values(process.argv).indexOf('--trustoria') > -1) {
-    basePath = '/rd/vhosts/trustoria/www/';
+    basePath = '/rd/vhosts/professions/trustoria/www/';
 } if (Object.values(process.argv).indexOf('--homeflock') > -1) {
     basePath = '/rd/vhosts/homeflock/www/';
 } if (Object.values(process.argv).indexOf('--bizstanding') > -1) {
