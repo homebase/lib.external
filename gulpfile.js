d0bd73d53389add8d50722bd78bdc425d4fa20ce
Yuriy Radov <y.radov@difive.com>
2019-08-06 14:02:10 +0300

    + hotfix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ if (process.argv[3] == '--c1') {
     basePath = '/rd/vhosts/persontrust.com/www/';
 } if (process.argv[3] == '--federal') {
     basePath = '/rd/vhosts/federal-data.com/www/';
-} if (Object.values(process.argv).indexOf('--farm_c') > -1) {
+} if (process.argv[3] && process.argv[3].substring(0,8) == '--farm_c') {
     basePath = '/rd/vhosts/farm/sites/c' + process.argv[3].substring(8) + '/www/';
 } if (Object.values(process.argv).indexOf('--homemetry') > -1) {
     basePath = '/rd/vhosts/address/homemetry.com/www/';
